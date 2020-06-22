bbdf0e4828aaff71067bef0f66155c93235fd8d1
Harisree <harisree.n.s@gmail.com>
2020-06-21 22:08:28 -0400

    updated the assignment

--- assignmnet.js
+++ assignmnet.js
@@ -2,3 +2,4 @@
 // Create a html page that displays each user info in a card format using js
 // Each card should contain - Name , eamil , address in a sentence 
 // Info should be displayed on loading the html page
+// add delete button to each user to delete the respective user from html
