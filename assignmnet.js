5590e4af5c00db436284f8ff38343f828e0519dc
Harisree <harisree.n.s@gmail.com>
2020-06-21 22:05:41 -0400

    new assignment

---
+++ assignmnet.js
@@ -0,0 +1,4 @@
+// Given is some user info in data.js
+// Create a html page that displays each user info in a card format using js
+// Each card should contain - Name , eamil , address in a sentence 
+// Info should be displayed on loading the html page
