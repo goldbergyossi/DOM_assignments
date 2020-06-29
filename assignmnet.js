bb73d6434f43d2dba3c03516f7fe8e95f6bd961f
Yossi <yygold@gmail.com>
2020-06-28 21:53:08 -0400

    using Fetch with an URL

--- assignmnet.js
+++ assignmnet.js
@@ -1,45 +1,42 @@
-// Given is some user info in data.js
-// Create a html page that displays each user info in a card format using js
-// Each card should contain - Name , eamil , address in a sentence 
-// Info should be displayed on loading the html page
-// add delete button to each user to delete the respective user from html
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((x) => x.json())
+  .then((response) => {
+    console.log(response)
+    processData(response)
+  });
 
-
-user.forEach((elem, index) => {
-  let parentDiv = document.querySelector('#user');
-  let childDiv = document.createElement('div');
+processData = (user) => {
+  user.forEach((elem, index) => {
+    let parentDiv = document.querySelector('#user');
+    let childDiv = document.createElement('div');
     childDiv.classList.add("box");
     childDiv.setAttribute("id", `user_${index}`)
-  let name = document.createElement('h3');
-  let email = document.createElement('h4');
-  let address = document.createElement('p');
-  let remove = document.createElement('button');
+    let name = document.createElement('h3');
+    let email = document.createElement('h4');
+    let address = document.createElement('p');
+    let remove = document.createElement('button');
 
-  name.innerText = elem.name;
-  email.innerText = elem.email;
-  address.innerText = `${elem.address.street} ${elem.address.suite} ${elem.address.city} ${elem.address.zipcode}`;
-  childDiv.appendChild(name);
-  childDiv.appendChild(email);
-  childDiv.appendChild(address);
-  childDiv.appendChild(remove);
-  parentDiv.appendChild(childDiv);
-  remove.innerText = "delete";
+    name.innerText = elem.name;
+    email.innerText = elem.email;
+    address.innerText = `${elem.address.street} ${elem.address.suite} ${elem.address.city} ${elem.address.zipcode}`;
+    childDiv.appendChild(name);
+    childDiv.appendChild(email);
+    childDiv.appendChild(address);
+    childDiv.appendChild(remove);
+    parentDiv.appendChild(childDiv);
+    remove.innerText = "delete";
 
 
   remove.addEventListener('click', (event) =>{
     console.log('delete user')
     deleteUser(event);
   })
-  
+
 })
-  
-  
-
-
+} 
 
 deleteUser = (event) => {
 console.log(event.target.parentNode.id); 
 var elem = document.getElementById(event.target.parentNode.id);
 elem.parentNode.removeChild(elem);
 }
-
